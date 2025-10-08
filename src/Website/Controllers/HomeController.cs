b4231ad649bc7783c8f05e1e2640d6fe9e21fdd6
Randy Oram <roram@cb.crateandbarrel.com>
2025-10-08 02:10:04 -0600

    checkpoint 4 commit

--- src/Website/Controllers/HomeController.cs
+++ src/Website/Controllers/HomeController.cs
@@ -56,17 +56,6 @@ public class HomeController : Controller
         return View(customerOrders);
     }
 
-    [HttpGet("/home/order/{orderId}")]
-    public async Task<IActionResult> OrderDetails(string orderId)
-    {
-        var details = await _customerOrderService.GetOrderDetails(orderId);
-
-        if (details is null || details.Count == 0)
-            return NotFound();
-
-        return View(details);
-    }
-
     public IActionResult Privacy() => View();
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
